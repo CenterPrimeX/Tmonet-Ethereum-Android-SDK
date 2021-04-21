5815b68d1539098e3778e55cbe82ad0f4ae70471
Ergashev Odiljon <oaergashev@gmail.com>
2021-04-21 14:21:50 +0900

    TMonet Ethereum SDK

---
+++ tmonet_ethereum_sdk/src/main/java/com/centerprime/tmonet_ethereum_sdk/util/RewardTransferResponseModel.java
@@ -0,0 +1,60 @@
+package com.centerprime.tmonet_ethereum_sdk.util;
+
+import com.google.gson.annotations.SerializedName;
+
+public class RewardTransferResponseModel {
+    @SerializedName("status")
+    private int status;
+    @SerializedName("message")
+    private String message;
+    @SerializedName("data")
+    private Data data;
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+
+    public class Data {
+        @SerializedName("tx_hash")
+        private String tx_hash;
+        @SerializedName("infura")
+        private String infura;
+
+        public String getInfura() {
+            return infura;
+        }
+
+        public void setInfura(String infura) {
+            this.infura = infura;
+        }
+
+        public String getTx_hash() {
+            return tx_hash;
+        }
+
+        public void setTx_hash(String tx_hash) {
+            this.tx_hash = tx_hash;
+        }
+    }
+}
